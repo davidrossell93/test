da7db41f8b9970365401b4a8da1bce3919925999
davidrossell93 <78764149+davidrossell93@users.noreply.github.com>
2024-11-14 04:33:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -33,11 +33,11 @@ document.addEventListener('DOMContentLoaded', function() {
         fullScreenContainer.style.display = 'none';
     });
 
-    // Configuración de partículas para el fondo principal con colores oscuros
+    // Configuración de partículas para el fondo principal con colores rojos terroríficos
     particlesJS('particles-js', {
         particles: {
             number: { value: 100, density: { enable: true, value_area: 700 } },
-            color: { value: ["#4b0082", "#ff0000", "#8b0000", "#191919"] }, // Colores oscuros y rojos
+            color: { value: ["#8b0000", "#ff0000", "#b22222", "#191919"] }, // Colores rojizos y oscuros
             shape: { type: 'circle', stroke: { width: 0, color: '#000000' } },
             opacity: { value: 0.4, random: true, anim: { enable: true, speed: 1, opacity_min: 0.1, sync: false } },
             size: { value: 3, random: true, anim: { enable: true, speed: 3, size_min: 0.1, sync: false } },
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
         particlesJS('particles-fullscreen', {
             particles: {
                 number: { value: 100, density: { enable: true, value_area: 700 } },
-                color: { value: ["#4b0082", "#ff0000", "#8b0000", "#191919"] }, // Mismos colores oscuros y rojos
+                color: { value: ["#8b0000", "#ff0000", "#b22222", "#191919"] }, // Mismos colores rojos terroríficos
                 shape: { type: 'circle', stroke: { width: 0, color: '#000000' } },
                 opacity: { value: 0.4, random: true, anim: { enable: true, speed: 1, opacity_min: 0.1, sync: false } },
                 size: { value: 3, random: true, anim: { enable: true, speed: 3, size_min: 0.1, sync: false } },
@@ -79,11 +79,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let colors = [
         [25, 25, 25],        // Gris oscuro
-        [75, 0, 130],        // Violeta oscuro (#4B0082)
-        [139, 0, 0]          // Rojo oscuro (#8B0000)
+        [139, 0, 0],         // Rojo oscuro (#8B0000)
+        [178, 34, 34]        // Rojo fuego (#B22222)
     ];
     let currentIndex = 0;
-    const stepsToViolet = 25;
+    const stepsToRed = 25;
     const stepsToBlack = 300;
 
     function interpolateColor(colorA, colorB, factor) {
@@ -97,7 +97,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function changeBackgroundColor() {
         let step = 0;
         const nextIndex = (currentIndex + 1) % colors.length;
-        const steps = currentIndex === 0 ? stepsToViolet : stepsToBlack;
+        const steps = currentIndex === 0 ? stepsToRed : stepsToBlack;
 
         const interval = setInterval(() => {
             const color = interpolateColor(colors[currentIndex], colors[nextIndex], step / steps);
