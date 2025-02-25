55c4bdbe0c7cc629d8eb9f17acbd6902d8da87c8
davidrossell93 <78764149+davidrossell93@users.noreply.github.com>
2025-02-25 05:17:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cargar Contenido a Través de Proxy</title>
+    <title>XD</title>
 </head>
 <body>
 
