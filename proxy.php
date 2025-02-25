99468c5cd93e6f04bfa80198ade454a6763067d9
davidrossell93 <78764149+davidrossell93@users.noreply.github.com>
2025-02-25 05:21:22 +0100

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -4,12 +4,14 @@ $url = 'https://xat.com/content/web/R00207/box/embed.html';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Para seguir redirecciones
 $response = curl_exec($ch);
-curl_close($ch);
 
-if ($response === FALSE) {
-    echo 'Error al cargar el contenido.';
+if (curl_errno($ch)) {
+    echo 'Error: ' . curl_error($ch);
 } else {
     echo $response;
 }
+
+curl_close($ch);
 ?>
